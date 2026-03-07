e56b05bdd67c44b6c7c79716d50aa41097553c66
Thohard <ringwelskirobin@gmail.com>
2026-03-07 17:11:15 +0100

    Frontend file-structure
    General file-structure for the frontend of the project

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("William er en Bot!");
+  res.send("Hello world!");
 });
 
 app.listen(3000, () => {
