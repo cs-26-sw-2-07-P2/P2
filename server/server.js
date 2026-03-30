f10ee7b0cacdd8b5a055b5a21a5f3ece37fbedbb
Thohard <ringwelskirobin@gmail.com>
2026-03-30 13:16:58 +0200

    Small db fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 // Serve frontend
 app.use(express.static('public'));
