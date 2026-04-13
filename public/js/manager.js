5285a69a817e381d862688baaa7c173b9adbe6b7
Tobias Axelsen <taxels25@student.aau.dk>
2026-04-13 14:36:12 +0200

    Manager.js and html work
    Tilføjelser til manager knapperne

--- public/js/manager.js
+++ public/js/manager.js
@@ -2,7 +2,13 @@ let questionnaire = document.querySelector(".questionnaire");
 
 questionnaire.addEventListener("click", createQuestionnaire);
 
-
+< button onclick="createQuestionnaire()">Create Questionnaire</button>
 function createQuestionnaire() {
     console.log("Button clicked");
 };
+
+document.getElementById("Create task").addEventListener("click", createTask);
+
+function createTask() {
+    window.location.href = "createtask.html";
+}
