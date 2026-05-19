c9d31abc2b923bd3c3b7ef5ef4c8ea6e94f9a746
Thohard <ringwelskirobin@gmail.com>
2026-05-19 13:10:09 +0200

    Team_viewer for Employees

--- server/server.js
+++ server/server.js
@@ -307,11 +307,35 @@ app.get("/api/employee-assignment", async (req, res) => {
     const assignment = await prisma.assignment.findUnique({
       where: { employeeId: employee.id },
       include: {
-        job: true,
+        job: {
+          include: {
+            assignments: {
+              include: {
+                employee: {
+                  include: {
+                   user: {
+                    select: {
+                      username: true
+                    }
+                   }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     });
 
-    res.json({ assignment });
+    const team = assignment.job.assignments.map(a => ({
+      employeeId: a.employeeId,
+      name: a.employee.user.username,
+      compatibility: a.compatibility,
+      priorityRank: a.priorityRank,
+      manuallyAdjusted: a.manuallyAdjusted
+    }));
+
+    res.json({ assignment, team });
 
   } catch (error) {
     console.error(error);
